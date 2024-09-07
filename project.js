27d860a71f92e5016d581cb6c1636a2bb019a5db
Ananthachai-palm <palmggxd@gmail.com>
2024-09-07 16:51:42 +0700

    Starter KIT

---
+++ project.js
@@ -0,0 +1,6 @@
+"use strict";
+function add(a, b) {
+    return a + b;
+}
+const result2 = add(1, 2);
+console.log(result2);
---
+++ project.ts
@@ -0,0 +1,8 @@
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+
+const result2 = add(1,2);
+console.log(result2)
+https://itpats-organization.gitbook.io/road-to-backend/type-script/react-with-type-script
